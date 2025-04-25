f509a83c9f6bcb8fc19222dd1f4981fb4a101974
Arslaan Akhtar <arslaanakhtar0351@gmail.com>
2025-04-25 11:21:53 +0530

    First-Git-Leet

---
+++ first_and_last_occur_leet.cpp
@@ -0,0 +1,59 @@
+class Solution
+{
+public:
+    vector<int> searchRange(vector<int> &nums, int target)
+    {
+
+        int start = 0, end = nums.size() - 1, first = -1, last = -1;
+        vector<int> ans(2);
+
+        while (start <= end)
+        {
+
+            int mid = start + (end - start) / 2;
+            // for left side
+
+            if (nums[mid] == target)
+            {
+                first = mid;
+                // left side aao;
+                end = mid - 1;
+            }
+
+            else if (nums[mid] < target)
+            {
+                start = mid + 1;
+            }
+            else
+            {
+                end = mid - 1;
+            }
+        }
+        start = 0, end = nums.size() - 1;
+        while (start <= end)
+        {
+            // for right side
+
+            int mid = start + (end - start) / 2;
+
+            if (nums[mid] == target)
+            {
+                last = mid;
+                // right side aao;
+                start = mid + 1;
+            }
+
+            else if (nums[mid] < target)
+            {
+                start = mid + 1;
+            }
+            else
+            {
+                end = mid - 1;
+            }
+        }
+        ans[0] = first;
+        ans[1] = last;
+        return ans;
+    }
+};
